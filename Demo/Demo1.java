4e2f1a577e0844250c6081fecffb1f2186d6ad93
Sivareddy0930 <sivareddy0930@gmail.com>
2023-07-28 19:14:31 +0530

    java

---
+++ Demo/Demo1.java
@@ -0,0 +1,17 @@
+
+ class Test{
+public void m1(){
+System.out.println("class2 method 1");
+}
+
+} 
+
+public class Demo1{
+	public static void main(String[] args){
+
+		Test obj=new Test();
+		obj.m1();
+		System.out.println("Hi,Bro");
+}
+}
+
--- One.java
+++ One.java
@@ -1,6 +1,5 @@
 class One {
 public static void main(String arg[]){
-
-		System.out.println("Hi Anna");
+	System.out.println(12);
 }
 }
