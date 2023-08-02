7f5c304ae2ecac163d707dd57fc9792eb737a544
Sivareddy0930 <sivareddy0930@gmail.com>
2023-08-02 11:09:08 +0530

    Variables

---
+++ Variables/Instancevariables.java
@@ -0,0 +1,22 @@
+package Variables;
+
+public class Instancevariables {
+     //instance variables .
+    //instance variables or non-static variables
+    //instance variables are different from object to object.
+    // if we update instance variable of one object it will not effect to another objects.
+    // created at inside class and outside method.
+    //it will access and update through only object.
+    int a=10;
+
+    public static void main(String[] args) {
+            Instancevariables obj=new Instancevariables();
+            System.out.println(obj.a);
+    }    
+}
+//to compile and execute we have use package related syntax here .
+// because we using package to store  our class.
+// to compile we need to mention  directory 
+//ex:- javac -d.Instancevariables.java
+// to execute we need package name.
+//eex:- java Variables.Instancevariables 
---
+++ Variables/Staticvariables.java
@@ -0,0 +1,24 @@
+package Variables;
+
+
+public class Staticvariables {
+
+    //static variable are also know as class variables.
+    //it can access directly in static context.
+    // it can also access through classname and object.
+    // it created with help of static keyword.
+    // single copy of static variable shared among all instances .
+    // if we update static variable in one object.it will effect in all objects.
+    //it get memeory at time of classloding at runtime by jvm.
+    //At time of classloading All static fields will get loaded and executed .After main method is called by JVM.
+    //heap memory stores Stati members. 
+    
+    static int a=100;
+    public static void main(String[] args) {
+        Staticvariables obj=new Staticvariables();
+        System.out.println(a);//Accessing directly.
+        System.out.println(Staticvariables.a);
+        System.out.println(obj.a);
+    }
+    
+}
