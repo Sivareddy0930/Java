e0cfb2df0b52ae16499deb80a520c61be45424dc
Sivareddy0930 <sivareddy0930@gmail.com>
2023-08-12 12:17:22 +0530

    scanner class

---
+++ ScannerClass/ScannerOne.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class ScannerOne{
+
+	public static void main(String[] args){
+
+		System.out.println("Scanner class");
+
+		  Scanner sc = new Scanner(System.in); 
+System.out.println("Enter Value:-");
+		int i=sc.nextInt();
+		System.out.println(i);
+	}
+
+}
