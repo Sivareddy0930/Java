42bc26bb57b3507553efa49f068cadeafaf0204e
Sivareddy0930 <sivareddy0930@gmail.com>
2023-08-14 17:38:25 +0530

    commandlineArgs

---
+++ UserdefineData/CommandLineArgs2.java
@@ -0,0 +1,12 @@
+
+
+class CommandLineArgs2{
+    public static void main(String[] args) {
+       
+       for(int i=0;i<args.length;i++){
+        System.out.println(args[i]);
+       }
+
+        //System.out.println(args.getClass().getName());
+    }
+}
