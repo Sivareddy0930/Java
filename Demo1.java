842a3ccab0ad915c6660b34720de6852045105ac
Sivareddy0930 <sivareddy0930@gmail.com>
2023-08-17 19:25:22 +0530

    Interface

--- Demo1.java
+++ Demo1.java
@@ -1,14 +1,26 @@
+abstract class A{
+	abstract void m1();
+}
+abstract class B{
+	abstract void m1();
+}
 
-class Demo1{
 
-	 static strictfp void m1(){}
-	
 
-	public static void main(String[] args){
-		
-		m1();
+public class Demo1 extends A,B{
 
+	public void m1(){
+		System.out.println("hi");
 	}
+
+    public static void main(String[] args) 
+    {
+       Demo1 obj=new Demo1();
+			obj.m1();
+
+		
+    }
+
 }
 
 
