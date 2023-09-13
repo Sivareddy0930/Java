2f461da883b2a95cb02c3f6880a99d16483f3634
Sivareddy0930 <sivareddy0930@gmail.com>
2023-09-13 15:05:52 +0530

    converstions

---
+++ Converstions/typeCasting.java
@@ -0,0 +1,14 @@
+
+
+class B{
+	public static void main(String[] args){
+			long i=170;
+			int b=(int)i;
+			char c='c';
+			byte k=(byte)c;
+			System.out.println(b);
+			System.out.println(k);
+	}
+}
+ 
+
