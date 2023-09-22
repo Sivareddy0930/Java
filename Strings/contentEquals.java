dd7108653ff24e9b081f847cbfe05129a3d2e5dc
Sivareddy0930 <sivareddy0930@gmail.com>
2023-09-22 15:14:06 +0530

    contentEquals methods

---
+++ Strings/contentEquals.java
@@ -0,0 +1,8 @@
+class contentEquals{
+    public static void main(String args[]){
+        String s="Siva";
+        StringBuffer sb=new StringBuffer("Siva");
+        //contentEquals() method 
+        System.out.println(s.contentEquals(sb));
+    }
+}
